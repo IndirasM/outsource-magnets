d1599515613508a687dd9e952eb2686f11cb6aa0
IndirasM <I.Maziukas@gmail.com>
2020-05-29 02:59:33 +0300

    add login basics

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,15 +7,18 @@ import { SettingsComponent } from "./main-container/settings/settings.component"
 import { NotFoundComponent } from './not-found/not-found.component';
 import { AddTrainingComponent } from './main-container/add-training/add-training.component';
 import { TrainingDetailsComponent } from './main-container/training-graph/training-details/training-details.component';
+import { AuthGuard } from './auth.guard';
 
 const routes: Routes = [
-  { path: "login", component: LoginComponent },
-  { path: "calendar", component: TrainingCalendarComponent },
-  { path: "graph", component: TrainingGraphComponent },
-  { path: "settings", component: SettingsComponent },
-  { path: "add-training", component: AddTrainingComponent },
-  { path: "details/:id", component: TrainingDetailsComponent },
-  { path: "**", component: NotFoundComponent }
+  { path: 'login', component: LoginComponent },
+  { path: '', canActivate: [AuthGuard], children: [
+    { path: "calendar", component: TrainingCalendarComponent },
+    { path: "graph", component: TrainingGraphComponent },
+    { path: "settings", component: SettingsComponent },
+    { path: "add-training", component: AddTrainingComponent },
+    { path: "details/:id", component: TrainingDetailsComponent }
+  ]},
+  { path: '**', component: NotFoundComponent }
 ];
 
 @NgModule({
