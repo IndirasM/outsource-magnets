babd2bdb2d408f405d345aad1f1c88f94c4d1c4c
IndirasM <I.Maziukas@gmail.com>
2020-05-29 14:06:12 +0300

    fix: move towards correct table

--- src/app/main-container/training-graph/training-details/training-details.component.ts
+++ src/app/main-container/training-graph/training-details/training-details.component.ts
@@ -15,7 +15,7 @@ export class TrainingDetailsComponent implements OnInit {
   currentTraining: any;
   loading: boolean = true;
 
-  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
+  displayedColumns: string[] = ['name', 'date'];
   dataSource: MatTableDataSource<any>;
   @ViewChild(MatPaginator, {static: true}) paginator: MatPaginator;
 
@@ -44,29 +44,29 @@ export class TrainingDetailsComponent implements OnInit {
 export interface PeriodicElement {
   name: string;
   position: number;
-  weight: number;
+  date: number;
   symbol: string;
 }
 
 const ELEMENT_DATA: PeriodicElement[] = [
-  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
-  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
-  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
-  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
-  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
-  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
-  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
-  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
-  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
-  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
-  {position: 11, name: 'Sodium', weight: 22.9897, symbol: 'Na'},
-  {position: 12, name: 'Magnesium', weight: 24.305, symbol: 'Mg'},
-  {position: 13, name: 'Aluminum', weight: 26.9815, symbol: 'Al'},
-  {position: 14, name: 'Silicon', weight: 28.0855, symbol: 'Si'},
-  {position: 15, name: 'Phosphorus', weight: 30.9738, symbol: 'P'},
-  {position: 16, name: 'Sulfur', weight: 32.065, symbol: 'S'},
-  {position: 17, name: 'Chlorine', weight: 35.453, symbol: 'Cl'},
-  {position: 18, name: 'Argon', weight: 39.948, symbol: 'Ar'},
-  {position: 19, name: 'Potassium', weight: 39.0983, symbol: 'K'},
-  {position: 20, name: 'Calcium', weight: 40.078, symbol: 'Ca'},
+  {position: 1, name: 'Hydrogen', date: 1.0079, symbol: 'H'},
+  {position: 2, name: 'Helium', date: 4.0026, symbol: 'He'},
+  {position: 3, name: 'Lithium', date: 6.941, symbol: 'Li'},
+  {position: 4, name: 'Beryllium', date: 9.0122, symbol: 'Be'},
+  {position: 5, name: 'Boron', date: 10.811, symbol: 'B'},
+  {position: 6, name: 'Carbon', date: 12.0107, symbol: 'C'},
+  {position: 7, name: 'Nitrogen', date: 14.0067, symbol: 'N'},
+  {position: 8, name: 'Oxygen', date: 15.9994, symbol: 'O'},
+  {position: 9, name: 'Fluorine', date: 18.9984, symbol: 'F'},
+  {position: 10, name: 'Neon', date: 20.1797, symbol: 'Ne'},
+  {position: 11, name: 'Sodium', date: 22.9897, symbol: 'Na'},
+  {position: 12, name: 'Magnesium', date: 24.305, symbol: 'Mg'},
+  {position: 13, name: 'Aluminum', date: 26.9815, symbol: 'Al'},
+  {position: 14, name: 'Silicon', date: 28.0855, symbol: 'Si'},
+  {position: 15, name: 'Phosphorus', date: 30.9738, symbol: 'P'},
+  {position: 16, name: 'Sulfur', date: 32.065, symbol: 'S'},
+  {position: 17, name: 'Chlorine', date: 35.453, symbol: 'Cl'},
+  {position: 18, name: 'Argon', date: 39.948, symbol: 'Ar'},
+  {position: 19, name: 'Potassium', date: 39.0983, symbol: 'K'},
+  {position: 20, name: 'Calcium', date: 40.078, symbol: 'Ca'},
 ];
