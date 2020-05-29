d3cc9c2443660a6acb5182cba7b43947df14258f
IndirasM <I.Maziukas@gmail.com>
2020-05-29 12:44:09 +0300

    feat: add graph mapping

--- src/app/main-container/training-graph/training-details/training-details.component.ts
+++ src/app/main-container/training-graph/training-details/training-details.component.ts
@@ -1,19 +1,31 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute } from "@angular/router";
+import { GraphService } from "src/app/graph.service";
+import { throwError } from "rxjs";
 
 @Component({
-  selector: 'app-training-details',
-  templateUrl: './training-details.component.html',
-  styleUrls: ['./training-details.component.scss']
+  selector: "app-training-details",
+  templateUrl: "./training-details.component.html",
+  styleUrls: ["./training-details.component.scss"],
 })
 export class TrainingDetailsComponent implements OnInit {
-
   id: string;
+  currentTraining: any;
 
-  constructor(private route: ActivatedRoute) { }
+  constructor(
+    private route: ActivatedRoute,
+    private graphService: GraphService
+  ) {}
 
   ngOnInit(): void {
-    this.id = this.route.snapshot.params['id'];
+    this.id = this.route.snapshot.params["id"];
+    this.graphService.fetchTraining(this.id).subscribe(
+      (data: any) => {
+        this.currentTraining = data;
+      },
+      (err) => {
+        throwError(err);
+      }
+    );
   }
-
 }
--- src/app/types.ts
+++ src/app/types.ts
@@ -2,3 +2,19 @@ export class UserCredentials {
     username: string;
     password: string;
 }
+
+export interface UnmappedEntity {
+    id: number,
+    name: string,
+    parentId: number
+}
+
+export interface GraphEntity {
+    id: string,
+    label: string
+}
+
+export interface GraphLink {
+    source: number,
+    target: number
+}
