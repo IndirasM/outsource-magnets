aa3fe9e8b6db505c5ef88da469f9d85b800843b5
IndirasM <I.Maziukas@gmail.com>
2020-05-29 13:41:15 +0300

    feat: add preliminary subject page

--- src/app/graph.service.ts
+++ src/app/graph.service.ts
@@ -16,6 +16,20 @@ export class GraphService {
         name: "Dude Duderson",
         teamId: 2
       }
+    ],
+    comments: [
+      {
+        name: 'keker',
+        text: 'oof'
+      },
+      {
+        name: 'keker2',
+        text: 'Article nor prepare chicken you him now. Shy merits say advice ten before lovers innate add. She cordially behaviour can attempted estimable. Trees delay fancy noise manor do as an small. Felicity now law securing breeding likewise extended and. Roused either who favour why ham. '
+      },
+      {
+        name: 'keker3',
+        text: 'oof'
+      }
     ]
   }
 
