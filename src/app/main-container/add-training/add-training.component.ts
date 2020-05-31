bd7e8238ee16200488a06811e87389dd3cbfc64a
IndirasM <I.Maziukas@gmail.com>
2020-05-31 19:52:53 +0300

    add training modal

--- src/app/main-container/add-training/add-training.component.ts
+++ src/app/main-container/add-training/add-training.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-add-training',
@@ -7,9 +9,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddTrainingComponent implements OnInit {
 
-  constructor() { }
+  newTrainingForm: FormGroup;
+  options = [
+    'kek',
+    'kekistan'
+  ]
+
+  constructor(
+    public dialogRef: MatDialogRef<AddTrainingComponent>,
+    @Inject(MAT_DIALOG_DATA) public data
+  ) { }
 
   ngOnInit(): void {
+    this.newTrainingForm = new FormGroup({
+      training: new FormGroup({
+        subject: new FormControl(''),
+        date: new FormControl('')
+      })
+    })
   }
 
+  onNewTopic() {
+    this.dialogRef.close();
+  }
+
+  onSubmit() {
+    this.dialogRef.close(this.newTrainingForm);
+  }
 }
