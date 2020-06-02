690b3b362258c4ef730d5f121af1d1828684607b
Tadas Kizevicius <tadkizevicius@gmail.com>
2020-06-02 05:23:28 +0300

    feat: suggested subjects

--- src/app/main-container/add-training/add-training.component.ts
+++ src/app/main-container/add-training/add-training.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit, Inject } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { GraphService } from 'src/app/graph.service';
+import {CalendarService} from '../../calendar.service';
+import {LearningDays} from '../../app.const';
 
 @Component({
   selector: 'app-add-training',
@@ -14,20 +16,30 @@ export class AddTrainingComponent implements OnInit {
   learningSubjects;
   subject;
   loading: boolean;
+  suggestedSubjects: SuggestedSubjects[] = [];
+  learningDays: LearningDays[] = [];
+  filteredSuggestedSubjects: SuggestedSubjects[] = [];
 
   constructor(
     public dialogRef: MatDialogRef<AddTrainingComponent>,
     @Inject(MAT_DIALOG_DATA) public data,
-    private graphService: GraphService
+    private graphService: GraphService,
+    private calendarService: CalendarService
   ) { }
 
   ngOnInit(): void {
+    console.log(this.data);
+    this.getSuggestedSubjects();
+    this.graphService.fetchAllTrainings().subscribe(learningSubjects => {
+      this.learningSubjects = learningSubjects;
+    });
+
     this.newTrainingForm = new FormGroup({
       training: new FormGroup({
         subject: new FormControl('', Validators.required),
         date: new FormControl('', Validators.required)
       })
-    })
+    });
 
     this.graphService.fetchAllTrainings().subscribe(learningSubjects => {
       this.learningSubjects = learningSubjects;
@@ -40,11 +52,11 @@ export class AddTrainingComponent implements OnInit {
       this.loading = true;
       this.graphService.fetchTraining(this.data.learningDay.subjectId).subscribe(res => {
         this.subject = res;
-        if(this.data.event && this.data.event.start) {
+        if (this.data.event && this.data.event.start) {
           this.newTrainingForm.get('training').get('date').setValue(this.data.event.start);
         }
         this.newTrainingForm.get('training').get('subject').setValue(this.learningSubjects.find(sub => sub.id === this.subject.id));
-      }, undefined, () => { this.loading = false })  
+      }, undefined, () => { this.loading = false; });
     }
   }
 
@@ -55,4 +67,32 @@ export class AddTrainingComponent implements OnInit {
   onSubmit() {
     this.dialogRef.close(this.newTrainingForm.value);
   }
+
+  getSuggestedSubjects() {
+    this.graphService.fetchSuggestedSubjects().subscribe(
+      resp => {
+        this.suggestedSubjects = resp;
+      }
+    ),
+      error => {
+        console.log(error.error.message);
+      };
+    this.calendarService.getLearningDays().subscribe(
+      res => {
+        this.learningDays = res;
+        this.filteredSuggestedSubjects = this.suggestedSubjects.filter((elem) => !this.learningDays.find(({ subjectId }) => elem.subjectId === subjectId));
+      },
+      error => {
+        console.log(error.error.message);
+      }
+    );
+
+
+  }
+
+}
+
+export interface SuggestedSubjects {
+  subjectId: number;
+  subjectName: string;
 }
